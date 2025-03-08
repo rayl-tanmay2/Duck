68d7a30330b0629ff5c0512054a59152d53d7ff1
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-03-08 17:15:04 +0530

    Create RubberDuck.java

---
+++ RubberDuck.java
@@ -0,0 +1,16 @@
+public class RubberDuck extends Duck{
+    public  RubberDuck(){
+        quackBehaviour = new Squeak();
+        swimBehaviour = new Floating();
+        flyBehaviour = new CannotFly();
+    }
+
+
+    @Override
+    public void display() {
+        System.out.println("I am a rubber duck!!");
+    }
+}
+
+
+
